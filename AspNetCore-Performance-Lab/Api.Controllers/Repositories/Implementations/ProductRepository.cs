07e74fb6ebb70ef62000c30535ba79c422dd0b98
armaan9991 <armaangill9991@gmail.com>
2026-07-16 09:17:27 -0600

    implemented Product Repository

--- AspNetCore-Performance-Lab/Api.Controllers/Repositories/Implementations/ProductRepository.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Repositories/Implementations/ProductRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace Api.Controllers.Repositories.Implementations
+﻿using Shared.Models;
+using Api.Controllers.Repositories.Interfaces;
+namespace Api.Controllers.Repositories.Implementations
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly List<Product> _products =
+        [
+            new Product
+            {
+                Id =1,
+                Name = "LapTop",
+                Price = 1200,
+                Category = "Electronics"
+            },
+            new Product
+            {
+                Id =2,
+                Name = "Keyboard",
+                Price = 80,
+                Category = "Accessories"
+            },
+            new Product
+            {
+                Id =3,
+                Name = "Mouse",
+                Price = 50,
+                Category = "Accessories"
+            },
+            new Product
+            {
+                Id =4,
+                Name = "Monitor",
+                Price = 300,
+                Category = "Electronics"
+            }
+        ];
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products;
+        }
+        public Product? GetById(int id)
+        {
+            return _products.FirstOrDefault(x => x.Id == id);
+        }
+        public Product Add(Product product)
+        {
+            _products.Add(product);
+            return product;
+        }
     }
 }
