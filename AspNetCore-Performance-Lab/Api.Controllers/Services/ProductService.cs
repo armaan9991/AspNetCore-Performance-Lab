f371b5a83288349ab61c85672b9d791561b13e76
armaan9991 <armaangill9991@gmail.com>
2026-07-16 09:08:14 -0600

    implemented Product service layer

--- AspNetCore-Performance-Lab/Api.Controllers/Services/ProductService.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Services/ProductService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Api.Controllers.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared.Models;
 
 namespace Api.Controllers.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IProductRepository _repository;
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _repository.GetAll();
+        }
+        public Product? GetProductById(int id)
+        {
+            return _repository.GetById(id);
+        }
+        public Product AddProduct(Product product)
+        {
+            return _repository.Add(product);
+        }
     }
 }
