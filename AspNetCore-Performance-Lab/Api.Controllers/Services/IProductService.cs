ebac40d6bdf011998fc5f9550775f25e1e0f4686
armaan9991 <armaangill9991@gmail.com>
2026-07-16 08:35:27 -0600

    created Product service interface

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Shared.Models;
+namespace Api.Controller.Services;
+
+public interface IProductService
+{
+    IEnumerable<Product> GetAllProducts();
+    Product? GetProductById(int id);
+    Product AddProduct(Product product);
+}
