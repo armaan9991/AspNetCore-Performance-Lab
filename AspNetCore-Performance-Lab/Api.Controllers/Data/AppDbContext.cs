61390507e964a9d6fe99aa38d205b2719b989933
armaan9991 <armaangill9991@gmail.com>
2026-07-17 15:47:16 -0600

    added comments to appdbcontext file

--- AspNetCore-Performance-Lab/Api.Controllers/Data/AppDbContext.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Data/AppDbContext.cs
@@ -2,12 +2,12 @@
 using Shared.Models;
 namespace Api.Controllers.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext   // inherit all EF core DbContext capabilites
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)   // DBcontextoptions tells which database, connection string 
         {
 
         }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }   // products table
     }
 }
