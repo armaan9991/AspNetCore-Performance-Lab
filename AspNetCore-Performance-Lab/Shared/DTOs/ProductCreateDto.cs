eeddddda9c3d1e08e82e8830a227e3a22f7eb8c6
armaan9991 <armaangill9991@gmail.com>
2026-07-22 12:47:40 -0600

    created CreateDto.

---
+++ AspNetCore-Performance-Lab/Shared/DTOs/ProductCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class ProductCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
