854e3a8d6087fab487c5d48e56bedfe45046ab2f
armaan9991 <armaangill9991@gmail.com>
2026-07-22 12:46:13 -0600

    added readDTO

---
+++ AspNetCore-Performance-Lab/Shared/DTOs/ProductReadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class ProductReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
