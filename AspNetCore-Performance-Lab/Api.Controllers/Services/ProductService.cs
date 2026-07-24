c5d57b4d1ff7474fa5a09ccf93b0aa0f97ccee7f
armaan9991 <armaangill9991@gmail.com>
2026-07-23 21:16:07 -0600

    updated  product service woth createDTO

--- AspNetCore-Performance-Lab/Api.Controllers/Services/ProductService.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Api.Controllers.Repositories.Interfaces;
+using Shared.DTOs;
 using Shared.Models;
 
 namespace Api.Controllers.Services
@@ -18,19 +19,24 @@ namespace Api.Controllers.Services
         {
             return await _repository.GetByIdAsync(id);
         }
-        public async Task<Product> AddProductAsync(Product product)
+        public async Task<Product> AddProductAsync(ProductCreateDto product)
         {
             if (product.Price<0)
             {
                 throw new Exception("no negative price");
             }
-            var item = await _repository.GetByIdAsync(product.Id);
-            if(item != null)
+            var item = await _repository.SearchByNameAsync(product.Name);
+            if (item != null)
             {
                 throw new Exception("Already present!!");
             }
-
-            return await _repository.AddAsync(product);
+            var productEntity = new Product
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Category = product.Category
+            };
+            return await _repository.AddAsync(productEntity);
         }
         public async Task<IEnumerable<Product>> GetByCategoryAsync(string category)
         {
