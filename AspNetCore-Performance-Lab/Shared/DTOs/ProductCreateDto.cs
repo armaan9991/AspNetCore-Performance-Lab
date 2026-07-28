9d7850bb3e78f4b6aba231a7df716845dfe03678
armaan9991 <armaangill9991@gmail.com>
2026-07-28 12:29:00 -0600

    added validation in DTOs

--- AspNetCore-Performance-Lab/Shared/DTOs/ProductCreateDto.cs
+++ AspNetCore-Performance-Lab/Shared/DTOs/ProductCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace Shared.DTOs
 {
     public class ProductCreateDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+        [Range(0.1,10000)]
         public decimal Price { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Category { get; set; } = string.Empty;
     }
 }
--- AspNetCore-Performance-Lab/Shared/DTOs/UpdateProductDto.cs
+++ AspNetCore-Performance-Lab/Shared/DTOs/UpdateProductDto.cs
@@ -4,7 +4,13 @@ namespace Shared.DTOs;
 
 public class ProductUpdateDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [Range(0.1, 10000)]
     public decimal Price { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string Category { get; set; } = string.Empty;
 }
