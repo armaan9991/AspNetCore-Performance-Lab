f63307c3029b6d0d523a1571b2bec6d96e85233f
armaan9991 <armaangill9991@gmail.com>
2026-07-28 13:33:24 -0600

    implemented Exception Middleware

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Net;
+using System.Text.Json;
+
+namespace Api.Controllers.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                //context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                var response = new ApiResponse<string>
+                {
+                    Success = false,
+                    Message = ex.Message,
+                    Data = null
+                };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        }
+    }
+}
