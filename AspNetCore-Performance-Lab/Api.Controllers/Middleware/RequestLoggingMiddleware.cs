06505eb0e41bb19b6a103bc6493eb8b5b6c3a4af
armaan9991 <armaangill9991@gmail.com>
2026-08-02 17:56:01 -0600

    implemented custom requestlogging middleware .

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Api.Controllers.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        public readonly RequestDelegate _next;
+        public readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation(
+                "Incoming Request : {Method} {Path}",
+                context.Request.Method,
+                context.Request.Path);
+
+            await _next(context);
+
+            _logger.LogInformation(
+                "Response Status: {StatusCode}",
+                context.Response.StatusCode);
+        }
+    }
+}
