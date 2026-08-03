77eecdb82148926d58f8ee139bcd85534ed0da4e
armaan9991 <armaangill9991@gmail.com>
2026-08-02 18:03:18 -0600

    implemented appsettings for pagination and caching implementation.

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Settings/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Controllers.Settings
+{
+    public class AppSettings
+    {
+        public int PageSize { get; set; } = 20;
+        public  string CompanyName { get; set; }= string.Empty;
+        public bool EnableCaching { get; set; };
+
+	}
+}
