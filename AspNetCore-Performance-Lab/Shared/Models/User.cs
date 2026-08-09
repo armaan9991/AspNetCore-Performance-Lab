b4f0deb8cb13014dd67815d7a848b40a826c8849
armaan9991 <armaangill9991@gmail.com>
2026-08-08 18:58:02 -0600

    added User entity.

---
+++ AspNetCore-Performance-Lab/Shared/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; }
+    }
+}
