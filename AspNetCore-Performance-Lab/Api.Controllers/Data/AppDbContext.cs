f122d99ca2cd51d6dc7109c21bf5a35f7da611c7
armaan9991 <armaangill9991@gmail.com>
2026-08-10 19:28:22 -0600

    added description to modelBuilder method.

--- AspNetCore-Performance-Lab/Api.Controllers/Data/AppDbContext.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Data/AppDbContext.cs
@@ -14,8 +14,11 @@ namespace Api.Controllers.Data
         // Make Each users email Unique
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // before doing custom configuration, we call base.OnModelCreating to ensure
+            // that any default configurations are applied first.
             base.OnModelCreating(modelBuilder);
-
+            // we have to configure User entity and then we check create index for DB as email for al users and then make it unique .
+            // so now two users cannot have same email address in the database.
             modelBuilder.Entity<User>().
                 HasIndex(u => u.Email).IsUnique();
         }
