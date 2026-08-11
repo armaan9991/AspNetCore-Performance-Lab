4ff228d8d36d8117504d2cf2c40697cd58e2f052
armaan9991 <armaangill9991@gmail.com>
2026-08-10 19:42:07 -0600

    added one method in user repo .

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Api.Controllers.Data;
+using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Api.Controllers.Repositories.Implementations
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User?>  GetByIdAsync (int id) {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+}
