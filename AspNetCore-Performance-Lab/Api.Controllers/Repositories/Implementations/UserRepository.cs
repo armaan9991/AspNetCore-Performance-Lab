3fda92f0b4d90d369c6ee186204971d3292f92b4
armaan9991 <armaangill9991@gmail.com>
2026-08-10 19:51:16 -0600

    implemented user repository.

--- AspNetCore-Performance-Lab/Api.Controllers/Repositories/Implementations/UserRepository.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Repositories/Implementations/UserRepository.cs
@@ -14,4 +14,21 @@ namespace Api.Controllers.Repositories.Implementations
         public async Task<User?>  GetByIdAsync (int id) {
             return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
         }
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        public async Task<bool?> EmailExistsAsync(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+
+    }
 }
