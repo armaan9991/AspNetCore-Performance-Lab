085e08062c958711d252a8275914fb7c802124ec
armaan9991 <armaangill9991@gmail.com>
2026-08-10 19:35:16 -0600

    implemented User Repo interface.

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Repositories/Interfaces/IUserRespository.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Models;
+
+namespace Api.Controllers.Repositories.Interfaces
+{
+    public interface IUserRespository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<User> AddAsync(User user);
+        Task<bool> EmailExistsAsync(string email);
+    }
+}
