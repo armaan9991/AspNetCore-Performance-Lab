7b2b5732450b59d04f145fb8bc9ac55d81bc29e0
armaan9991 <armaangill9991@gmail.com>
2026-08-11 21:09:58 -0600

    Created Auth Service with methods not implemented.

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Api.Controllers.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace Api.Controllers.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRespository _userRespository;
+        private readonly PasswordHasher<User> _passwordHasher;
+        public AuthService(IUserRespository userRespository, PasswordHasher<User> passwordHasher)
+        {
+            _userRespository = userRespository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<AuthResponseDto?> LoginAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
