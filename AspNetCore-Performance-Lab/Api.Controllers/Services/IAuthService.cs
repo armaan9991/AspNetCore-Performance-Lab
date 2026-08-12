ffefe888d35924a431ed1c5e528cd47c21bde9ca
armaan9991 <armaangill9991@gmail.com>
2026-08-11 21:04:04 -0600

    implemented IAutService interface and changed logindto property.

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs;
+
+namespace Api.Controllers.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
+        Task<AuthResponseDto?> LoginAsync(LoginDto loginDto);
+    }
+}
--- AspNetCore-Performance-Lab/Shared/DTOs/LoginDto.cs
+++ AspNetCore-Performance-Lab/Shared/DTOs/LoginDto.cs
@@ -9,14 +9,14 @@ using System.ComponentModel.DataAnnotations;
 namespace Shared.DTOs
 {
     public class LoginDto
-	{
+    {
 
-		[Required]
-		[EmailAddress]
-		public string Email { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
 
-		[Required]
-		public string Password { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
 
-	}
+    }
 }
