ea7539de97b9c2b549212bf5dc6405ae5aaca085
armaan9991 <armaangill9991@gmail.com>
2026-08-25 22:04:12 -0600

    Add AuthController with login and register endpoints

--- AspNetCore-Performance-Lab/Api.Controllers/Controllers/AuthController.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Controllers/AuthController.cs
@@ -1,6 +1,32 @@
-﻿namespace Api.Controllers.Controllers
+﻿using Api.Controllers.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+
+namespace Api.Controllers.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController:ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("regsiter")]
+        [AllowAnonymous] // to allow anonymous people to login or register.
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var result = await _authService.RegisterAsync(registerDto);
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+            return Ok(result);
+        }
     }
 }
