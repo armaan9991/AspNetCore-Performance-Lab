5a979d16e8ab5ea7201c23481ee2978cd1d44ff2
armaan9991 <armaangill9991@gmail.com>
2026-08-25 21:50:57 -0600

    Add UserAlreadyExistsException for better error handling

---
+++ AspNetCore-Performance-Lab/Api.Controllers/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Controllers.Exceptions
+{
+    public class UserAlreadyExistsException: Exception
+    {
+    public UserAlreadyExistsException(string message) : base(message) { }
+    }
+}
