8c8ca02aa077c1fbcfce753439328554d5cc0aaf
armaan9991 <armaangill9991@gmail.com>
2026-08-25 22:10:19 -0600

    Add ProductQueryDto class for pagination and filtering

---
+++ AspNetCore-Performance-Lab/Shared/DTOs/ProductQueryDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class ProductQueryDto
+    {
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string? Category { get; set; }
+
+        public string? Search { get; set; }
+
+        public string? SortBy { get; set; }
+
+        public bool SortDescending { get; set; } = false;
+    }
+}
