557af70b138d6f6722f13cc8a8ee30872581487a
armaan9991 <armaangill9991@gmail.com>
2026-08-25 22:16:53 -0600

    Add PagedResponseDto<T> for standardized pagination

---
+++ AspNetCore-Performance-Lab/Shared/DTOs/PagedResponseDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class PagedResponseDto<T>
+    {
+        public IEnumerable<T> Items { get; set; } = [];
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public bool HasNextPage { get; set; }
+
+        public bool HasPreviousPage { get; set; }
+    }
+}
