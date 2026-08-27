9eb87d54c1d9507d7247050fbd1684a315f9f41c
armaan9991 <armaangill9991@gmail.com>
2026-08-27 14:10:34 -0600

    Add Swagger auth & benchmarking setup/cleanup methods

--- AspNetCore-Performance-Lab/Api.Controllers/Program.cs
+++ AspNetCore-Performance-Lab/Api.Controllers/Program.cs
@@ -15,6 +15,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    // 1. Define the Bearer Authentication scheme
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter your JWT token exactly like this: Bearer {your_token}"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 //builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllers();
--- AspNetCore-Performance-Lab/BenchmarkRunner/ApiBenchmark.cs
+++ AspNetCore-Performance-Lab/BenchmarkRunner/ApiBenchmark.cs
@@ -4,7 +4,28 @@ namespace MyBenchmarks;
 
 public class ApiBenchmark
 {
-    [Benchmark]
+    private HttpClient _httpClient = null!; // null as intialization but would be assigned before use.
+
+    [GlobalSetup]  // this tells donet to run exactly once before  any performance begins. handles logic outside of timer.
+    public void setup()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://localhost:5263");
+    }
+    [Benchmark]   // means this method will  test performance.
+    public async Task GetProducts()
+    {
+        var response = await _httpClient.GetAsync("/api/Product/paged?page=1&pageSize=10"); // hit actually api.
+        response.EnsureSuccessStatusCode();         // if there is any exception then this throw exception to stop the false beanchmark / measurements.
+    }
+
+    [GlobalCleanup]   // execute this method once all benchmarks are finished. closes all network socket.
+    public void Cleanup()
+    {
+        _httpClient.Dispose();
+    }
+
+    [Benchmark]        //  means it tells that this method is to test and measure.
     public void Test()
     {
         
